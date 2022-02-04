578e9b1f5104090dade4d54efc8fb3ca3f24666d
Oya-Tomo <tomo-san2618@outlook.jp>
2022-02-04 19:55:47 +0900

    add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="PySQL",
+    version="0.0.1",
+    author="Oya-Tomo",
+    description="This is the ORM of python sqlite.",
+    url="https://github.com/Oya-Tomo/PySQL",
+    packages=setuptools.find_packages
+)
